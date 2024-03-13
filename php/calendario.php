b3e4fb8744b3e3083eefa3e52fb854de5105d65f
EstebanValladares <131742625+EstebanValladares@users.noreply.github.com>
2024-03-12 19:51:34 -0600

    documentos nuevos

---
+++ php/calendario.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/c91ca5f5f4.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../style/styleOptions.css">
+    <!-- <link rel="stylesheet" href="style.css"> -->
+    <title>Calendario</title>
+</head>
+
+<body>
+    <main>
+        <article class="articleAll">
+            <section class="section-nav">
+                <div class="div1">
+                    <a href="http://localhost:8000/php/estudiantes.php"><i class="fa-solid fa-user iconUser"></i></a>
+                    <a href="http://localhost:8000/php/calendario.php"><i class="fa-solid fa-calendar iconCalendario"></i></a>
+                </div>
+                <div class="div2">
+                    <i class="fa-solid fa-gear"></i>
+                </div>
+            </section>
+            <section class="section-container">
+                <div class="container">
+                    <div class="calendar">
+                        <div class="header">
+                        <div class="month"></div>
+                        <div class="btns">
+                            <div class="btn today-btn">
+                            <i class="fas fa-calendar-day"></i>
+                            </div>
+                            <div class="btn prev-btn">
+                            <i class="fas fa-chevron-left"></i>
+                            </div>
+                            <div class="btn next-btn">
+                            <i class="fas fa-chevron-right"></i>
+                            </div>
+                        </div>
+                        </div>
+                        <div class="weekdays">
+                        <div class="day">Dom</div>
+                        <div class="day">Lun</div>
+                        <div class="day">Mar</div>
+                        <div class="day">Mie</div>
+                        <div class="day">Jue</div>
+                        <div class="day">Vie</div>
+                        <div class="day">Sab</div>
+                        </div>
+                        <div class="days">
+                        </div>
+                    </div>
+                    <button class="new_date">Nueva cita</button>
+                </div>
+                <div class="datesCits">
+                    <article class="list">
+                        <h3>Citas registradas</h3>
+                    </article>
+                </div>
+            </section>
+        </article>
+    </main>
+</body>
+    <script src="../js/app.js"></script>
+</html>
